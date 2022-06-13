f83d4b34a7712a035ba33cb5206f1b846bb4c564
illuzX <98640218+illuzX@users.noreply.github.com>
2022-06-13 08:39:50 +0530

    Initial commit

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/illuzX/AutoFilterTelegramBot.git /app
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /app
+fi
+cd /app
+pip3 install -U -r requirements.txt
+echo "STATUS:ONLINE.."
+python3 bot.py
