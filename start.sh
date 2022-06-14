fdeac670f0a86b41ecf170555f82e360fbc5d1d6
illuzX <98640218+illuzX@users.noreply.github.com>
2022-06-14 14:43:02 +0530

    update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/illuzX/AutoFilterTelegramBot.git /app
+  git clone https://github.com/illuzX/privaterepo /app
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /app
